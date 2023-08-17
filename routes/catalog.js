0ed50669cffffcfd6407e58258bda5fe134fc82b
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-08-17 13:47:37 +0100

    Implemented add category

--- routes/catalog.js
+++ routes/catalog.js
@@ -6,6 +6,10 @@ const instrument_controller = require('../controllers/instrumentController');
 
 router.get('/', category_controller.index);
 
+router.get('/category/add', category_controller.category_create_get);
+
+router.post('/category/add', category_controller.category_create_post);
+
 router.get('/category/:id', instrument_controller.instrument_list);
 
 module.exports = router;
