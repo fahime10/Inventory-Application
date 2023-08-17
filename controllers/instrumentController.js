200314b0d9cf41c48378bf064a3b33c115c12184
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-08-17 18:18:13 +0100

    Implemented update instrument

--- controllers/instrumentController.js
+++ controllers/instrumentController.js
@@ -2,6 +2,7 @@ const Instrument = require('../models/instrument');
 const Category = require('../models/category');
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
+const category = require('../models/category');
 
 exports.instrument_list = asyncHandler(async (req, res, next) => {
     const [category, intrumentsInCategory] = await Promise.all([
@@ -76,6 +77,94 @@ exports.instrument_create_post = [
         } else {
             await instrument.save();
             res.redirect('/catalog');
+        }
+    }),
+];
+
+exports.instrument_update_get = asyncHandler(async (req, res, next) => {
+    const [instrument, allCategories] = await Promise.all([
+        Instrument.findById(req.params.id).exec(),
+        Category.find().exec(),
+    ]);
+
+    if (instrument === null) {
+        const err = new Error('Category not found');
+        err.status = 404;
+        return next(err);
+    }
+
+    for (const category of allCategories) {
+        if (category._id.toString() === instrument.category.toString()) {
+            category.checked = 'true';
+        }
+    }
+
+    res.render('instrument_form', {
+        title: 'Update ' + instrument.name,
+        categories: allCategories,
+        instrument: instrument,
+    });
+});
+
+exports.instrument_update_post = [
+    (req, res, next) => {
+        if (!(req.body.category instanceof Array)) {
+            if (typeof req.body.category == 'undefined') {
+                req.body.category = [];
+            } else {
+                req.body.category = new Array(req.body.category);
+            }
+        }
+        next();
+    },
+
+    body('name')
+        .trim()
+        .escape(),
+
+    body('description')
+        .trim()
+        .escape(),
+
+    body('price')
+        .trim()
+        .escape(),
+
+    body('stock')
+        .trim()
+        .escape(),
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const instrument = new Instrument({
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+            stock: req.body.stock,
+            _id: req.params.id,
+        });
+
+        if (!errors.isEmpty()) {
+            const allCategories = await Category.find().exec();
+
+            for (const category of allCategories) {
+                if (instrument.category.includes(category._id)) {
+                    category.checked = 'true';
+                }
+            }
+
+            res.render('instrument_form', {
+                title: 'Add new instrument',
+                categories: allCategories,
+                instrument: instrument,
+                errors: errors.array(),
+            });
+            return;
+        } else {
+            await Instrument.findByIdAndUpdate(req.params.id, instrument, {});
+            res.redirect('/catalog/category/'+instrument.category);
         }
     }),
 ];
